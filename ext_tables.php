2b38dfbe4748c2be0f65003db5d742e7afd1d307
froemken <froemken@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-15 15:07:12 +0000

    - Bug: 14323: allow_nonadmins don't work
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3quixplorer/trunk@40222 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -1,18 +1,13 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-
 if (TYPO3_MODE=='BE')	{
+	$tmp_config = unserialize($_EXTCONF);
 	
-	t3lib_extMgm::addModule('tools','txt3quixplorerM1',"",t3lib_extMgm::extPath($_EXTKEY).'mod1/');
-
-
-
-
+	if($tmp_config["allow_nonadmins"] == 1){
+		t3lib_extMgm::addModule("user","txt3quixplorerM1","",t3lib_extMgm::extPath($_EXTKEY)."mod1/");
+	} else {
+		t3lib_extMgm::addModule("tools","txt3quixplorerM1","",t3lib_extMgm::extPath($_EXTKEY)."mod1/");
+	}
 }
-
-
-
-
-
 ?>
